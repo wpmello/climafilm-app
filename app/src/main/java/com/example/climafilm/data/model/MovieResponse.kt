5a4cfb586f851e4d2507e8c9687fea199814cb07
Wagner Mello <wagnermellow2k@gmail.com>
2023-08-30 20:24:12 -0300

    feature: added attribute in Movies class

--- app/src/main/java/com/example/climafilm/data/model/MovieResponse.kt
+++ app/src/main/java/com/example/climafilm/data/model/MovieResponse.kt
@@ -8,18 +8,27 @@ data class MovieResponse(
     val id: Int,
     val original_title: String,
     val overview: String,
-    val popularity: Int,
+    val popularity: Double,
+    val vote_average: Double,
+    val vote_count: Int,
+    val backdrop_path: String,
     val poster_path: String,
     val release_date: String,
     val title: String
 ) {
     fun toEntity(): Movie {
         return Movie(
+            adult = adult,
+            genre_ids = genre_ids,
             id = id,
-            genre_ids  = genre_ids,
+            original_title = original_title,
             overview = overview,
             popularity = popularity,
+            vote_average = vote_average,
+            vote_count = vote_count,
+            backdrop_path = backdrop_path,
             poster_path = poster_path,
+            release_date = release_date,
             title = title
         )
     }
--- app/src/main/java/com/example/climafilm/domain/model/Movie.kt
+++ app/src/main/java/com/example/climafilm/domain/model/Movie.kt
@@ -1,9 +1,11 @@
 package com.example.climafilm.domain.model
 
 data class Movie(
+    val adult: Boolean,
     val id: Int,
     val genre_ids: List<Int>,
     val overview: String,
+    val original_title: String,
     val popularity: Double,
     val poster_path: String,
     val release_date: String,
