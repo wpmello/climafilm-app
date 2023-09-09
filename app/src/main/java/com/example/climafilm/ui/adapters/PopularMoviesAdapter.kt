45b142a5839fbe3137b86561a5cc9b3717001e32
Wagner Mello <wagnermellow2k@gmail.com>
2023-09-09 16:28:28 -0300

    feature: created another movie lists of home fragment

---
+++ app/src/main/java/com/example/climafilm/ui/adapters/PopularMoviesAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.climafilm.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
+import com.example.climafilm.databinding.MovieItemRicyclerViewBinding
+import com.example.climafilm.domain.model.Movie
+import com.example.climafilm.util.Constants.Companion.BASE_IMAGE_URL
+import javax.inject.Inject
+
+class PopularMoviesAdapter @Inject constructor(
+    private val requestManager: RequestManager
+) : RecyclerView.Adapter<PopularMoviesAdapter.ViewHolder>() {
+
+    private lateinit var movieList: List<Movie>
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = MovieItemRicyclerViewBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val movie = movieList[position]
+        holder.bind(movie)
+    }
+
+    override fun getItemCount(): Int = movieList.size
+
+    fun setList(list: List<Movie>) {
+        movieList = list
+        notifyDataSetChanged()
+    }
+
+    inner class ViewHolder(private val binding: MovieItemRicyclerViewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(movie: Movie) {
+            requestManager.load(BASE_IMAGE_URL + movie.poster_path).into(binding.imageMovie)
+            binding.txtMovieName.text = movie.title
+        }
+    }
+}
---
+++ app/src/main/java/com/example/climafilm/ui/viewmodels/PopularMoviesViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.climafilm.ui.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.climafilm.data.model.Poster
+import com.example.climafilm.domain.usecase.GetPopularMoviesUseCase
+import com.example.climafilm.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import javax.inject.Inject
+
+@HiltViewModel
+class PopularMoviesViewModel @Inject constructor(
+    private val popularMoviesUseCase: GetPopularMoviesUseCase
+) : ViewModel() {
+
+    private val _movieList = MutableLiveData<Resource<Poster>>()
+    var movieList: LiveData<Resource<Poster>> = _movieList
+
+    init {
+        getPopularMovies()
+    }
+
+    fun getPopularMovies() = viewModelScope.launch {
+        _movieList.postValue(Resource.Loading())
+        val response = popularMoviesUseCase.invoke()
+        _movieList.postValue(handleMovieResponse(response))
+    }
+
+
+    private fun handleMovieResponse(response: Response<Poster>): Resource<Poster> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+}
