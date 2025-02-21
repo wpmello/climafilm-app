f6f4853b217c04e9e368ab87daed00c61c89bda9
wagne <wagnermellow2k@gmail.com>
2025-02-21 19:08:07 -0300

    refactoring splash and onboard screen to jetpack compose

--- app/src/main/java/com/example/climafilm/ui/fragments/onboarding/SplashFragment.kt
+++ app/src/main/java/com/example/climafilm/ui/fragments/onboarding/SplashFragment.kt
@@ -1,34 +1,34 @@
 package com.example.climafilm.ui.fragments.onboarding
 
-import android.content.Context
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.climafilm.R
+import com.example.climafilm.ui.fragments.components.SplashScreen
 
 class SplashFragment : Fragment() {
+    private lateinit var composeVIew: ComposeView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        Handler().postDelayed({
-            if (onBoardingFinished()) {
-                findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
-            } else {
-                findNavController().navigate(R.id.action_splashFragment_to_onBoardingViewPagerFragment)
-            }
-        }, 3000)
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+    ): View {
+        return ComposeView(requireContext()).also {
+            composeVIew = it
+        }
     }
 
-    private fun onBoardingFinished(): Boolean {
-        val sharedPref =
-            requireActivity().getSharedPreferences("onBoardingFinished", Context.MODE_PRIVATE)
-        return sharedPref.getBoolean("Finished", false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        composeVIew.setContent {
+            SplashScreen(
+                onNavigateToHome = { findNavController().navigate(R.id.action_splashFragment_to_homeFragment) },
+                onNavigateToOnboardingViewPager = { findNavController().navigate(R.id.action_splashFragment_to_onBoardingViewPagerFragment) }
+            )
+        }
     }
 }
